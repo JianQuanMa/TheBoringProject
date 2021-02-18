04193fae36c61e14d7f3a81f3c6422e787f7e808
Jian Ma <jianquanma@gmail.com>
2021-02-17 19:40:50 -0500

    added the initial project

---
+++ TheBoringProject/TheBoringProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheBoringProject
+//
+//  Created by Jian Ma on 2/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
